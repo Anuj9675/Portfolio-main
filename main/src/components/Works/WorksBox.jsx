406483458ed743e947a5dbd8397b29ac4583b714
Anuj9675 <anujupadyay6265@gmail.com>
2024-02-06 12:27:56 +0530

    workBox.jsx

--- main/src/components/Works/WorksBox.jsx
+++ main/src/components/Works/WorksBox.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Works = [
   {
     id: 1,
-    img: 'https://cdn.discordapp.com/attachments/853242550243557408/1154302221287161876/Screenshot_92.png',
+    bg: 'https://cdn.discordapp.com/attachments/853242550243557408/1154302221287161876/Screenshot_92.png',
     title: "Todo App",
     desc: "Stay organized and work seamlessly with Todo App. Enjoy the flexibility of switching between Dark and Light theme while managing tasks.",
     link: "https://todo-app-o.vercel.app/",
@@ -14,7 +14,7 @@ const Works = [
   },
   {
     id: 2,
-    img: 'https://cdn.discordapp.com/attachments/853242550243557408/1141031721815527545/Screenshot_89.png',
+    bg: 'https://cdn.discordapp.com/attachments/853242550243557408/1141031721815527545/Screenshot_89.png',
     title: "Animal Welfare",
     desc: "I created a dummy charity website as my internship task with The Sparks Foundation which shows payment Gateway Integration.",
     link: "https://animalwelfare.netlify.app/",
@@ -25,10 +25,10 @@ const WorksBox = () => {
   return (
     <section className="my-10 container ">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 ">
-        {Works.map(({ id, img, title, desc, link, git }) => (
+        {Works.map(({ id, bg, title, desc, link, git }) => (
           <div
             key={id}
-            className={`bg-[url('${img}')] border border-black rounded-lg text-black dark:text-white`}
+            className={`bg-[url('${bg}')] border border-black rounded-lg text-black dark:text-white`}
           >
             
             <div className="p-3 md:p-16 backdrop-blur-sm space-y-3 rounded-xl ">

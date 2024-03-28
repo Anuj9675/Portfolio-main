d2a47ebb6b00f9747cd24887d1a6cdf1269a409e
Anuj Upadhyay <114591897+Anuj9675@users.noreply.github.com>
2024-03-28 20:51:21 +0530

    Update Baselayout.jsx

--- main/src/components/Baselayout.jsx
+++ main/src/components/Baselayout.jsx
@@ -3,6 +3,7 @@ import Navbar from "./navbar/Navbar";
 import Hero from "./hero/Hero";
 import About from "./about/About";
 import Works from "./Works/Works";
+import Contact from "./contacts/contact";
 import Footer from "./footer/Footer"
 import { Route, Routes } from "react-router-dom";
 
@@ -17,6 +18,7 @@ const Base = () => {
         <Route exact path={"/"} element={<Hero/>}/>
         <Route exact path={"/about"} element={<About/>} />
         <Route exact path={"/works"} element={<Works/>} />
+        <Route exact path={"/contact"} element={<Contact/>}/>
     </Routes>
     <Footer/>
     </>
